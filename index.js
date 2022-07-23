349e58dea21a1fdd16c20920d79b7c29df882ac0
Jaume Dellunde <jdellund@xtec.cat>
2022-07-23 11:52:00 +0200

    Foto

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
+const midaFoto = 1000;
 var usuari = false;
 var nom, contrasenya;
 var scriptURL = "https://script.google.com/macros/s/AKfycbwO6FnBnaBmxrzPRyLDhV8D6pW7Pe6V8-SYIdSHZXMmg8JqjJwQz8H8zA8wl3U6u2pW/exec"
+var foto_feta;
+
+window.onload = function() {    
+  document.getElementById("fitxer").addEventListener("change", function(event) {
+     readURL(this);
+  });
+}
+
+function readURL(input) { 
+    if(input.files[0] != undefined) {
+      let canvas = document.getElementById("canvas");
+      let ctx = canvas.getContext("2d");
+      let img = new Image;
+      img.src = URL.createObjectURL(input.files[0]);
+      img.onload = function() {
+          let iw=img.width;
+          let ih=img.height;
+          let scale=Math.min((midaFoto/iw),(midaFoto/ih));
+          let iwScaled=iw*scale;
+          let ihScaled=ih*scale;
+          canvas.width=iwScaled;
+          canvas.height=ihScaled;
+          ctx.drawImage(img,0,0,iwScaled,ihScaled);
+          canvas.style.display = "none";
+          document.getElementById("camera").style.display = "none";
+          document.getElementById("foto").src = canvas.toDataURL("image/jpeg",0.5);
+          document.getElementById("desar").style.display = "block";
+          foto_feta = canvas.toDataURL("image/jpeg",0.5);
+      }
+    } else {
+      console.log("Error: no s'ha obtingut cap fitxer.");
+    }
+}
+
+function desa_foto() {
+
+}
 
 function canvi_seccio(num_boto) {
     if (usuari) {
-        const menu = document.getElementById('menu');
+        const menu = document.getElementById("menu");
         const num_botons = menu.children.length;
         for (let i = 1; i < num_botons; i++) {
             let boto = document.getElementById("boto_" + i);
@@ -29,7 +67,6 @@ function sortida() {
         location.reload();
     }
 }
-
 
 function nou_usuari() {
     nom = document.getElementById("nom_usuari").value;
@@ -62,7 +99,7 @@ function nou_usuari() {
 function inici_sessio() {
     nom = document.getElementById("nom_usuari").value;
     contrasenya = document.getElementById("contrasenya").value;
-    let consulta = scriptURL + "?query=select&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
+    let consulta = scriptURL + "?query=select&from=usuaris&where=usuari&is=" + nom + "&and=contrasenya&equal=" + contrasenya;
     fetch(consulta)
         .then((resposta) => {
             return resposta.json();
